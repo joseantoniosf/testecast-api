ff2ce399fc1ff1a6773e3bee4fc4bc161e3f0b70
Antonio Filho <joseantoniosf84@gmail.com>
2019-09-27 11:00:03 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/example/testecast/api/TestecastApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.testecast.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecastApiApplication.class, args);
+	}
+
+}
