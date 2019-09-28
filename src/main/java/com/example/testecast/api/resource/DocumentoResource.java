8b703e11fc48c0d721b39999da8ab6b796498fbe
Antonio Filho <joseantoniosf84@gmail.com>
2019-09-28 13:08:23 -0300

    Inserindo e buscando DocumentoLeft

--- src/main/java/com/example/testecast/api/resource/DocumentoResource.java
+++ src/main/java/com/example/testecast/api/resource/DocumentoResource.java
@@ -1,12 +1,20 @@
 package com.example.testecast.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.testecast.api.model.DocumentoLeft;
 import com.example.testecast.api.model.DocumentoRight;
@@ -23,14 +31,33 @@ public class DocumentoResource {
 	@Autowired
 	private DocumentoRightRepository documentoRightRepository;
 	
-	@GetMapping("{codigo}/left")
+	@GetMapping("/left")
 	public List<DocumentoLeft> listarLeft() {
 		return documentoLeftRepository.findAll();
 	}
 
-	@GetMapping("{codigo}/right")
+	@GetMapping("/right")
 	public List<DocumentoRight> listarRight() {
 		return documentoRightRepository.findAll();
 	}
 
+	@PostMapping("{codigo}/left")
+	public ResponseEntity<DocumentoLeft> criar(@PathVariable Long codigo, @RequestBody DocumentoLeft docLeft, HttpServletResponse response) {
+		DocumentoLeft documentoLeft = new DocumentoLeft();
+		documentoLeft.setCodigo(codigo);
+		documentoLeft.setDocumento(docLeft.getDocumento());
+		
+		DocumentoLeft docLeftSalvo = documentoLeftRepository.save(documentoLeft);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().build().toUri();
+		
+		return ResponseEntity.created(uri).body(docLeftSalvo);
+	}
+	
+	@GetMapping("/{codigo}/left")
+	public ResponseEntity<DocumentoLeft> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<DocumentoLeft> docLeft = documentoLeftRepository.findById(codigo);
+		return docLeft.isPresent() ?
+				ResponseEntity.ok(docLeft.get()) : ResponseEntity.notFound().build();
+	}
 }
